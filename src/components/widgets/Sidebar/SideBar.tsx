b274a732d1c74117c89bbfa32fb7ed74bee90506
w01f <arsenimarchenko@bk.ru>
2024-08-09 18:39:29 +0300

    fix: Remove fetch response from SideBar component

--- src/components/widgets/Sidebar/SideBar.tsx
+++ src/components/widgets/Sidebar/SideBar.tsx
@@ -1,13 +1,8 @@
 import { FC } from "react";
 import styles from "./sidebar.module.scss";
-import { FoldersApi } from "@/services/folders.api";
-import { cookies } from "next/headers";
 
-const SideBar: FC = async () => {
-  const cookieStore = cookies();
-  const test = await FoldersApi.getAll(cookieStore);
-
-  return <aside className={styles.sidebar}>{JSON.stringify(test)}</aside>;
+const SideBar: FC = () => {
+  return <aside className={styles.sidebar}></aside>;
 };
 
 export default SideBar;
