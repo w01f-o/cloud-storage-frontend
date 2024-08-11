071531af23cd31c6709e855c600b044a1f73e9a8
w01f <arsenimarchenko@bk.ru>
2024-08-11 18:37:39 +0300

    feat: New autoprefixer library for post css

--- postcss.config.js
+++ postcss.config.js
@@ -2,24 +2,17 @@ module.exports = {
   plugins:
     process.env.NODE_ENV === "production"
       ? [
-          "postcss-flexbugs-fixes",
-          [
-            "postcss-preset-env",
-            {
-              autoprefixer: {
-                flexbox: "no-2009",
-              },
-              stage: 3,
-              features: {
-                "custom-properties": false,
-              },
-            },
-          ],
           [
             "postcss-pxtorem",
             {
               rootValue: 16,
               propList: ["*"],
+            },
+          ],
+          [
+            "autoprefixer",
+            {
+              flexbox: "no-2009",
             },
           ],
         ]
