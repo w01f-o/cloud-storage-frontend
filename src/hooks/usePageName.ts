bba4bbd9befc56f5cdc1be7203a00b141f3b3f08
w01f <arsenimarchenko@bk.ru>
2024-08-11 18:29:20 +0300

    feat: usePageName hook
    This hook returned of the page name, if appRoutes array have this name

--- src/hooks/usePageName.ts
+++ src/hooks/usePageName.ts
@@ -3,5 +3,6 @@ import { appRoutes } from "@/routes/routes";
 
 export const usePageName = (): string | undefined => {
   const pathname = usePathname();
+
   return appRoutes.find((route) => route.path === pathname)?.name;
 };
