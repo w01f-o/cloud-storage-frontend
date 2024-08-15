678ce0ed907308529b1f38274b423e95c642756b
w01f <arsenimarchenko@bk.ru>
2024-08-15 20:12:30 +0300

    feat: dev info in profile page

--- src/components/pages/profile/Profile.tsx
+++ src/components/pages/profile/Profile.tsx
@@ -4,7 +4,30 @@ import { auth } from "@/services/auth/auth";
 const Profile: FC = async () => {
   const session = await auth();
 
-  return <div>{session?.user.name}</div>;
+  const devInfo = () => {
+    const array = [];
+    for (const key in session!.user) {
+      if (session) {
+        array.push({
+          key,
+          //@ts-expect-error
+          value: session.user[key],
+        });
+      }
+    }
+
+    return array;
+  };
+
+  return (
+    <div>
+      {devInfo().map((el) => (
+        <div key={el.key} style={{ fontSize: 20, marginTop: 6 }}>
+          {el.key}: {el.value}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Profile;
