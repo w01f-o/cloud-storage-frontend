0fd360412a1271e16930028d83cc9a9c7fcac58f
w01f <arsenimarchenko@bk.ru>
2024-09-07 02:28:37 +0300

    fix: Type error
    ./src/components/widgets/Files/LastUploadedFiles/LastUploadedFiles.tsx:21:33
    Type error: Type '{ file: File; dict: RootDictionary; extended: false; }' is not assignable to type 'IntrinsicAttributes & FileProps'.

--- src/components/widgets/Files/LastUploadedFiles/LastUploadedFiles.tsx
+++ src/components/widgets/Files/LastUploadedFiles/LastUploadedFiles.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { FilesApi } from "@/services/api/index.api";
 import File from "@/components/entities/File/File";
-import { getDictionary } from "@/actions/lang.action";
 import Link from "next/link";
 import { Col } from "@w01f-o/react-grid-layout";
 import styles from "./lastUploadedFiles.module.scss";
@@ -10,7 +9,6 @@ interface LastUploadedFilesProps {}
 
 const LastUploadedFiles: FC<LastUploadedFilesProps> = async ({}) => {
   const { data: files } = await FilesApi.getLastUploaded();
-  const dict = await getDictionary();
 
   return (
     <>
@@ -18,7 +16,7 @@ const LastUploadedFiles: FC<LastUploadedFilesProps> = async ({}) => {
         files.map((file) => (
           <Col xs={12} key={file.id}>
             <Link href={`/folder/${file.folderId}`}>
-              <File file={file} dict={dict} extended={false} />
+              <File file={file} extended={false} />
             </Link>
           </Col>
         ))
